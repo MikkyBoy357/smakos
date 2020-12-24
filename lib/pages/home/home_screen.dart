e9397c463139e50796075a60bde942d46586375c
Michael Olusegun <mikkyboy2005@gmail.com>
2020-12-25 00:11:54 +0100

    BottomNavigationBar updated

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -1,39 +1,101 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:shop_app/constants.dart';
-import 'package:shop_app/screens/home/components/body.dart';
+import 'package:online_shopping/constants.dart';
+import 'package:online_shopping/pages/home/components/body.dart';
+import 'package:online_shopping/widgets/custom_text_feld.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(),
+      backgroundColor: Colors.white,
+      appBar: buildAppBar(context),
       body: Body(),
+      bottomNavigationBar: Container(
+        height: 60,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey[200],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(children: [
+                  Icon(
+                    Icons.home,
+                    color: Colors.orange,
+                  ),
+                  Text(
+                    'Home',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.orange,
+                    ),
+                  )
+                ]),
+                Column(
+                  children: [
+                    Icon(Icons.list_alt_rounded),
+                    Text(
+                      'Categories',
+                      style: TextStyle(fontSize: 12),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.rss_feed_outlined),
+                    Text(
+                      'Feed',
+                      style: TextStyle(fontSize: 12),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Icon(Icons.account_circle_outlined),
+                    Text(
+                      'Account',
+                      style: TextStyle(fontSize: 12),
+                    )
+                  ],
+                ),
+                Column(children: [
+                  Icon(Icons.feedback_outlined),
+                  Text(
+                    'Feedback',
+                    style: TextStyle(fontSize: 12),
+                  )
+                ]),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 
-  AppBar buildAppBar() {
+  AppBar buildAppBar(BuildContext context) {
     return AppBar(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.red,
+      automaticallyImplyLeading: false,
       elevation: 0,
-      leading: IconButton(
-        icon: SvgPicture.asset("assets/icons/back.svg"),
-        onPressed: () {},
+      title: SearchBar(
+        icon: Icon(
+          Icons.search,
+          color: Colors.black,
+        ),
+        hintText: 'Search on Smakos',
       ),
       actions: <Widget>[
         IconButton(
           icon: SvgPicture.asset(
-            "assets/icons/search.svg",
-            // By default our  icon color is white
-            color: kTextColor,
-          ),
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: SvgPicture.asset(
             "assets/icons/cart.svg",
             // By default our  icon color is white
-            color: kTextColor,
           ),
           onPressed: () {},
         ),
